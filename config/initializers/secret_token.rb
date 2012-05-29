c69795f767c310b8eca6194f41e1995210848b88
Ivan Kraev <kukurua.tuni@gmail.com>
2012-05-30 01:23:54 +0400

    Beginning

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dao::Application.config.secret_token = '56a24cb74ff1cdba5a79dc6d7bf77c3e1258900965120d6c5a8b28012d3c991667bb8533df3ae2469b45c158765711588d4cc190c7b8ebd0de67824f6a4ffdc9'
